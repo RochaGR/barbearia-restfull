cdb1639cc0e4d76021f2498b7375bb486ebe1cb4
RochaGR <gutop.rocha@gmail.com>
2025-08-13 21:08:23 -0300

    Primeiro Comit

---
+++ src/main/java/com/br/barbeariaRest/BarbeariaRestApplication.java
@@ -0,0 +1,13 @@
+package com.br.barbeariaRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbeariaRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbeariaRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/barbeariaRest/BarbeariaRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.barbeariaRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbeariaRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
