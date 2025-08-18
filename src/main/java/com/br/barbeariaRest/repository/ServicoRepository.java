7955d76ac6e2909391f7e044125d45a7fd1874e4
RochaGR <gutop.rocha@gmail.com>
2025-08-18 19:55:22 -0300

    Repository do servico pronto

---
+++ src/main/java/com/br/barbeariaRest/repository/ServicoRepository.java
@@ -0,0 +1,12 @@
+package com.br.barbeariaRest.repository;
+
+
+import com.br.barbeariaRest.model.Servico;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ServicoRepository  extends JpaRepository<Servico, Integer> {
+
+}
+
